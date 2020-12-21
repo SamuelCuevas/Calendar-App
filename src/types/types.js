00e12aa2acb39649af2c4409febd165c9f62b243
Samuel Cuevas <samuelignacio46@gmail.com>
2020-12-21 00:04:21 -0300

    Calendar-App

---
+++ src/types/types.js
@@ -0,0 +1,27 @@
+
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+
+    eventSetActive: '[event] Set Active',
+    eventLogout: '[event] Logout event',
+
+    eventStartAddNew: '[event] Start add new',
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+    eventLoaded: '[event] Events loaded',
+    
+
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
+
+}
